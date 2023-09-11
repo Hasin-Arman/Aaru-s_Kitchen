554ce1d1728388b07dc3f19c557de03e37fc4f29
Hasin-Arman <hasinarman6@gmail.com>
2023-09-11 10:44:26 +0600

    commit

--- Best_Resturant/views.py
+++ Best_Resturant/views.py
@@ -4,9 +4,12 @@ from django.core.paginator import Paginator
 def home(request):
     if 'value' in request.POST:
         items=recipyModel.objects.filter(ingredients__contains=request.POST['value'])
+        paginator=Paginator(items, 1)
+        page=request.GET.get('page')
+        paged_items=paginator.get_page(page)
     else:   
         items=recipyModel.objects.all()
-        paginator=Paginator(items, 1)
+        paginator=Paginator(items, 2)
         page=request.GET.get('page')
         paged_items=paginator.get_page(page)
     return render(request, 'index.html', {'items':paged_items})
